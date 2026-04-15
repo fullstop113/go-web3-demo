40a71ffe3e1f017ee862463068069fe04d4ab21e
fullstop113 <623041273@qq.com>
2026-04-15 23:44:12 +0800

    完成路由设置，r.gourp设置组，api V1.POST设置请求解析handler

--- router/router.go
+++ router/router.go
@@ -1,17 +1,31 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/fullstop113/go-web3-demo/controller"
 	"github.com/fullstop113/go-web3-demo/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(middleware.JWTAuth())
-		
+
 	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{"ok": true})
 	})
+
+	apiV1 := r.Group("/api/v1")
+	{
+		// public routes
+		apiV1.POST("/register", controller.Register)
+		apiV1.POST("/login", controller.Login)
+
+		// private routes
+		private := apiV1.Group("/user")
+		private.Use(middleware.JWTAuth())
+		{
+			private.GET("/getUserInfo", controller.GetUserInfo)
+		}
+	}
 	return r
 }
 
